428bc4051ec7015cffda29d2804763b14aab1461
imranq2 <imranq2@hotmail.com>
2021-03-02 10:37:38 -0800

    use db name in folder name

--- backup.sh
+++ backup.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env sh
 
 # from https://github.com/Drivetech/mongodump-s3
-# Environent variables to set
+# Environment variables to set
 # MONGO_USER
 # MONDO_PASSWORD
 # MONGO_SERVER
 # MONGO_SERVER_PORT
+# MONGO_DB_NAME
 # S3_BUCKET
 # S3_PATH
 
 BACKUP_NAME="$(date -u +%Y-%m-%d_%H-%M-%S)_UTC.gz"
 CERT_FILE="/usr/local/bin/rds-combined-ca-bundle.pem"
 
+rm -rf "/backup/${MONGO_DB_NAME}"
+
 # Run backup
 mongodump --archive \
   --ssl \
   --uri "mongodb://${MONGO_USER}:${MONDO_PASSWORD}@${MONGO_SERVER}:${MONGO_SERVER_PORT}/?tls=true&tlsCAFile=${CERT_FILE}&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false" \
-	--out=/backup/dump \
-	--db=fhir_dev
+	--out="/backup/dump/${MONGO_DB_NAME}" \
+	--db="${MONGO_DB_NAME}"
 
 # Compress backup
-cd /backup/ && tar -cvzf "${BACKUP_NAME}" dump
+cd "/backup/${MONGO_DB_NAME}" && tar -cvzf "${BACKUP_NAME}" dump
 # Upload backup
-aws s3 cp "/backup/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
+aws s3 cp "/backup/${MONGO_DB_NAME}/${BACKUP_NAME}" "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}"
 # Delete temp files
-rm -rf /backup/dump
+rm -rf "/backup/${MONGO_DB_NAME}/dump"
